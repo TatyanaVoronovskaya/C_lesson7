5d8bc4b0695cfb556a63e2486c6b72ec22255d9b
Tatyana <vta278@mail.ru>
2023-07-12 23:11:10 +0300

    добавлена задача3 среднее арифм в столбце массива

---
+++ task7_2/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] Create2DArray(int rows, int cols)
+{
+    int[,] array2D = new int[rows, cols];
+    Random rnd = new Random();
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            array2D[i, j] = rnd.Next(0, 50);
+        }
+    }
+    return array2D;
+}
+
+void ShowArray(int[,] arr2D)
+{
+    for (int i = 0; i < arr2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr2D.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr2D[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedString = Console.ReadLine();
+    int number = Convert.ToInt32(inputedString);
+    return number;
+}
+
+void FindMediumCols(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        double avarege = sum / (array.GetLength(0));
+        System.Console.WriteLine($"среднее арифм столбца {j + 1} = {avarege:f2} ");
+    }
+    System.Console.WriteLine();
+}
+
+int rows = ReadInt("введите количество строк ");
+int cols = ReadInt("введите количество столбцов ");
+int[,] array2D = Create2DArray(rows, cols);
+ShowArray(array2D);
+FindMediumCols(array2D);
